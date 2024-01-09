e6cbe66aaaebb31f3e7af6cde25aede79a28acfe
sk8marco10 <marco_0.o@hotmail.com>
2024-01-08 22:15:11 -0500

    second

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Agenda de Datos</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+</head>
+<body>
+  <div class = "container">
+    <div class="jumbotron">
+      <h1 class="display-4">Agenda de Datos</h1>
+      <p class="lead">Aplicacion de muestra de una Agenda</p>
+      <hr class="my-4">
+      <p>PHP aplicacion de muestra conectada a una base de datos MySQL para mostrar una Agenda</p>
+    </div>
+    <table class="table table-striped table-responsive">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Direccion</th>
+          <th>Telefono Fijo</th>
+          <th>Telefono Movil</th>
+          <th>Correo Electronico</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+
+        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "BD");
+
+        $cadenaSQL = "select * from agenda";
+        $resultado = mysqli_query($conexion, $cadenaSQL);
+
+        while ($fila = mysqli_fetch_object($resultado)) {
+         echo "<tr><td> " .$fila->nombre . 
+         "</td><td>" . $fila->apellido .
+         "</td><td>" . $fila->direccion .
+         "</td><td>" . $fila->telefijo .
+         "</td><td>" . $fila->telemovil .
+         "</td><td>" . $fila->email .
+         "</td></tr>";
+       }
+       ?>
+     </tbody>
+   </table>
+ </div>
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+</html>
+
